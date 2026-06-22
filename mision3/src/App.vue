1f23ca2560c508276f681643df00b2fbf2ba828e
arnauavanza <arnau@avanzaprl.es>
2026-06-22 09:51:31 +0200

    feat: added loanding process

--- mision3/src/App.vue
+++ mision3/src/App.vue
@@ -2,6 +2,7 @@
   <div class="flex flex-col gap-5 rounded-md border border-gray-300 p-5 shadow">
     <h1>PokéRandom</h1>
     <Random @click="randomPokemonButton" />
+    <Loader v-if="loading" />
     <PokemonInfo v-if="pokemon" :pokemon="pokemon" />
   </div>
 </template>
@@ -9,19 +10,37 @@
 <script setup lang="ts">
 import PokemonInfo from './components/pokemon_info.vue'
 import Random from './components/random.vue'
+import Loader from './components/loader.vue'
 import { functions } from './composable/functions';
 import { ref, onMounted } from 'vue';
 import type { Pokemon } from './interfaces/types';
 
 const pokemon = ref<Pokemon | null>(null)
+const loading = ref(true)
 
 onMounted(async () => {
-  const { randomPokemon } = functions()
-  pokemon.value = await randomPokemon()
+  try {
+    const { randomPokemon } = functions()
+
+    pokemon.value = await randomPokemon()
+  } catch (error) {
+    console.error(error)
+  } finally {
+    loading.value = false
+  }
 })
 
 async function randomPokemonButton() {
   const { randomPokemon } = functions()
-  pokemon.value = await randomPokemon()
+  loading.value = true
+
+  try {
+    pokemon.value = await randomPokemon()
+  } catch (error) {
+    console.error(error)
+  } finally {
+    loading.value = false
+  }
 }
+
 </script>
--- mision3/src/components/random.vue
+++ mision3/src/components/random.vue
@@ -1,7 +1,7 @@
 <template>
   <form action="">
     <button type="button" @click="handleClick"
-      class="cursor-pointer border-none bg-amber-500 p-2.5 text-base text-white">Random</button>
+      class="cursor-pointer border bg-amber-500 p-2.5 text-base text-white">Random</button>
   </form>
 </template>
 
