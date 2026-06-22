b09b6938d6d1d2e1233fce5966c5f8cd87f3f891
arnauavanza <arnau@avanzaprl.es>
2026-06-22 10:24:22 +0200

    feat: added .env and adapted code

--- mision3/src/composable/functions.ts
+++ mision3/src/composable/functions.ts
@@ -1,10 +1,14 @@
 import type { Pokemon } from '../interfaces/types.ts';
 import axios from 'axios';
 
+const API_URL: string = import.meta.env.VITE_API_URL;
+
 export function functions() {
     async function randomPokemon(): Promise<Pokemon> {
+        console.log(import.meta.env);
+        console.log(import.meta.env.VITE_API_URL);
         const randomId = Math.floor(Math.random() * 898) + 1;
-        const randomPokemon = await axios.get(`https://pokeapi.co/api/v2/pokemon/${randomId}`);
+        const randomPokemon = await axios.get(`${API_URL}${randomId}`);
         const pokemon: Pokemon = {
             imageUrl: randomPokemon.data.sprites.front_default,
             name: randomPokemon.data.name,
