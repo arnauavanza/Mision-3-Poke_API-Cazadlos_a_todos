e37bf631fea593bc360520f00d54401d51c2d882
arnauavanza <arnau@avanzaprl.es>
2026-07-01 12:23:13 +0200

    refactor: removed type any and deconstructed axios respone

--- mision4/src/composable/functions.ts
+++ mision4/src/composable/functions.ts
@@ -1,4 +1,4 @@
-import type { Pokemon } from '../interfaces/types.ts';
+import type { Pokemon, PokeAPIResponse } from '../interfaces/types.ts';
 import axios from 'axios';
 
 const API_URL: string = import.meta.env.VITE_API_URL;
@@ -6,13 +6,14 @@ const API_URL: string = import.meta.env.VITE_API_URL;
 export function useRandomPokemon() {
     async function randomPokemon(): Promise<Pokemon> {
         const randomId = Math.floor(Math.random() * 898) + 1;
-        const randomPokemon = await axios.get(`${API_URL}${randomId}`);
+        const { data } = await axios.get<PokeAPIResponse>(`${API_URL}${randomId}`);
         const pokemon: Pokemon = {
-            imageUrl: randomPokemon.data.sprites.front_default,
-            name: randomPokemon.data.name,
-            types: randomPokemon.data.types.map((type: any) => type.type.name),
+            imageUrl: data.sprites.front_default,
+            name: data.name,
+            types: data.types.map((type: { type: { name: string } }) => type.type.name),
         };
         return pokemon;
     }
+
     return { randomPokemon };
 }
